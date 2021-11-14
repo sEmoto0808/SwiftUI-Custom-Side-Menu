d2dbacb2c254694a0f5fe54d447fa6097619c9a0
Sho Emoto <dd2d00seg1@gmail.com>
2021-11-14 18:06:38 +0900

    add project

---
+++ Shared/SwiftUI_Custom_Side_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Custom_Side_MenuApp.swift
+//  Shared
+//
+//  Created by Sho Emoto on 2021/11/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Custom_Side_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
